93dc20f77b69ee043282a23e460c2505b1086e87
Stefany Izidio <izidiostefany@gmail.com>
2025-08-16 00:39:05 -0300

    feat: Adição do handler global

---
+++ src/main/java/com/produtosapi/produtosAPI/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.produtosapi.produtosAPI.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Captura erros de validação @Valid
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField() , error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    // Captura erros de JSON malformado ou corpo vazio
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidJson(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Corpo da requisição inválido ou incompleto");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
